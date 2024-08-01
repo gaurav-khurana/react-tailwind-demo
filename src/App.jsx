ea7e540cb4b0d62a25f3e8cef29e9c45bf2e0ce6
Gaurav <program.drgk@gmail.com>
2024-08-01 10:48:05 -0700

    Initial commit with config for tailwind

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-start items-center mt-10 bg-gray-300 ">
+      <Navbar />
+
+      <div className="flex flex-row justify-center align-middle mt-[25vh] mb-5">
+        <a className="mx-11" href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1 className="text-3xl font-mono font-bold text-orange-500">
+        Vite + React + Tailwind CSS
+      </h1>
+
+      <div className="flex flex-col justify-center items-center py-4 my4">
+        <div className="mt-5">
+          <button
+            className="px-4 py-2 w-[10vw] border-red-500 bg-teal-500 hover:bg-amber-700 hover:text-white transition-all duration-300 hover:rounded-md "
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Count {count}
+          </button>
+          <button
+            className="px-4 py-2 ml-10 w-[10vw] border-red-500 bg-amber-500 hover:bg-amber-700 hover:text-white transition-all duration-300 hover:rounded-md "
+            onClick={() => setCount(0)}
+          >
+            Reset
+          </button>
+        </div>
+
+        <p className="text-cyan-800 mt-10 mb-5 font-medium font-mono">
+          This is a demo using React with Tailwind CSS
+        </p>
+      </div>
+
+      <p className="read-the-docs w-[50vw] ml-20 text-left font-mono ">
+        This is a custom component library for Reusable React components like
+        <br />
+        Navbar, <br /> Sidebar (Discord style),
+        <br /> Dark mode toggle switch with custom hook
+      </p>
+
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors"); // bonus color that can be imported frm tailwind
+
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // add custom colors in tailwind config file
+        primary: "#202225",
+        secondary: "#5865f2",
+        gray: colors.trueGray, // import trueGray colors from tailwind
+        gray: {
+          // overide trueGray colors to discord colors
+          900: "#202225",
+          800: "#2f3136",
+          700: "#36393f",
+          600: "#4f545c",
+          400: "#d4d7dc",
+          300: "#e5e3e8",
+          200: "#ebedef",
+          100: "#f2f3f5",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
